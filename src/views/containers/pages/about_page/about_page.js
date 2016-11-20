83c05ed0082c33ef086820575c02f85a45455553
michaelbenin <michaellouisbenin@gmail.com>
2016-11-20 16:45:57 -0500

    [MISC] updated copy in about

--- src/views/containers/pages/about_page/about_page.js
+++ src/views/containers/pages/about_page/about_page.js
@@ -5,6 +5,10 @@ export default function () {
   return (
     <div className="about-page">
       <h1>{'What\'s this about?'}</h1>
+      <p>
+        This project aims to do one thing well: make server side rendering simple
+        in a react application using only mature community maintained libraries.
+      </p>
       <Link to={'/repo/michaelBenin/react-ssr-spa'}>react-ssr-spa</Link>
     </div>
   );
