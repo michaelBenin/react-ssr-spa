12daf5bb34469173a5a27012c743341ca4530c0a
michaelbenin <michaellouisbenin@gmail.com>
2016-12-06 16:22:09 -0500

    [BUGFIX] - hot reloading reducers

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -34,7 +34,7 @@ export function configureStore(history, initialState = {}, env) {
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
-      const nextReducer = require('../reducers'); // eslint-disable-line global-require
+      const nextReducer = require('../reducers').default; // eslint-disable-line global-require
       store.replaceReducer(nextReducer);
     });
   }
