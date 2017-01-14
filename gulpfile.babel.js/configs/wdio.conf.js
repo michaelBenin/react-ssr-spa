389de0298f7fe6c02f1a287142baaed256695217
michaelbenin <michaellouisbenin@gmail.com>
2017-01-13 22:33:00 -0500

    [FEATURE] - add browsers and platforms for desktop

--- gulpfile.babel.js/configs/wdio.conf.js
+++ gulpfile.babel.js/configs/wdio.conf.js
@@ -1,4 +1,6 @@
 // http://webdriver.io/guide/testrunner/configurationfile.html
+// browser config stolen from vue
+// https://github.com/vuejs/vue/blob/dev/build/karma.sauce.config.js
 
 const conf = {
   services: ['selenium-standalone'],
@@ -20,9 +22,47 @@ if (process.env.TRAVIS_CI) {
   conf.user = process.env.SAUCE_USERNAME;
   conf.key = process.env.SAUCE_ACCESS_KEY;
   conf.capabilities = [{
+    browserName: 'safari',
+    platform: 'OS X 10.10',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    name: 'react-ssr-spa-acceptance',
+    build: process.env.TRAVIS_BUILD_NUMBER
+  }, {
     browserName: 'chrome',
-    version: '42',
-    platform: 'XP',
+    platform: 'Windows 7',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    name: 'react-ssr-spa-acceptance',
+    build: process.env.TRAVIS_BUILD_NUMBER
+  }, {
+    browserName: 'firefox',
+    platform: 'Windows 7',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    name: 'react-ssr-spa-acceptance',
+    build: process.env.TRAVIS_BUILD_NUMBER
+  }, {
+    browserName: 'internet explorer',
+    version: '9',
+    platform: 'Windows 7',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    name: 'react-ssr-spa-acceptance',
+    build: process.env.TRAVIS_BUILD_NUMBER
+  }, {
+    browserName: 'internet explorer',
+    version: '10',
+    platform: 'Windows 8',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    name: 'react-ssr-spa-acceptance',
+    build: process.env.TRAVIS_BUILD_NUMBER
+  }, {
+    browserName: 'internet explorer',
+    version: '11',
+    platform: 'Windows 8.1',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    name: 'react-ssr-spa-acceptance',
+    build: process.env.TRAVIS_BUILD_NUMBER
+  }, {
+    browserName: 'MicrosoftEdge',
+    platform: 'Windows 10',
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
     name: 'react-ssr-spa-acceptance',
     build: process.env.TRAVIS_BUILD_NUMBER
