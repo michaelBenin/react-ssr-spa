c00c2b0430fdd10c57e67bbf567c7860bbc8b8e7
michaelbenin <michaellouisbenin@gmail.com>
2017-01-13 22:44:42 -0500

    [CONFIG] - increase test timeout

--- test/acceptance/spec/title_test.js
+++ test/acceptance/spec/title_test.js
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 
 describe('webdriver.io page', function () {
   it('should have the right server rendered title', function () {
+    this.timeout(30000);
     browser.url('http://127.0.0.1:8001/repo/michaelBenin/react-ssr-spa');
     const title = browser.getTitle();
     expect(title).to.equal('react-ssr-spa - Server side rendered single page app using reactjs official libraries.');
