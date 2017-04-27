a2e412041980696e7b305675f004bd4116099c06
michaelbenin <michaellouisbenin@gmail.com>
2017-04-27 14:04:23 -0400

    [MISC] - update libs and added back callback when run on server

--- src/react_router/route_state_managers/search_results_state_manager.js
+++ src/react_router/route_state_managers/search_results_state_manager.js
@@ -2,21 +2,31 @@ import { canUseDOM } from 'exenv';
 import fetchSearchAction from '../../redux/actions/async_search_actions';
 
 export default function fetchSearchData(store) {
-  return function fetchData(nextState, replace) {
+  return function fetchData(nextState, replace, callback) {
     const { dispatch } = store;
     // run async
     if (canUseDOM) {
       if (store.getState().config.initialPageLoad === true) {
+        if(callback) {
+          return callback();
+        }
         return false;
       }
       fetchSearchAction(nextState.params.query, store)(dispatch);
+      if(callback) {
+        return callback();
+      }
       return false;
     }
     // run sync
     return fetchSearchAction(nextState.params.query, store)(dispatch)
-      .then(callback)
+      .then(function cb() { if(callback) {
+        return callback();
+      }
+        return false; })
       .catch(function handleServerSideRenderError(err) {
-
+        // log error
+        callback(err);
       });
   };
 }
