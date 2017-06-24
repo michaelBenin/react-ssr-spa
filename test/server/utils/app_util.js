1fbbf2539edcab7347e5698ac16f35fab9bfbcde
michaelbenin <michaellouisbenin@gmail.com>
2017-06-24 14:39:41 -0400

    [MISC] - example of testing different hosts with supertest

--- test/server/utils/app_util.js
+++ test/server/utils/app_util.js
@@ -1,4 +1,26 @@
 import supertest from 'supertest';
 import { app } from '../../../src/server/services/express_service';
 
-export default supertest.agent(app);
+function createVhostTester(supertestApp, vhost) {
+  const real = supertest(supertestApp);
+  const proxy = {};
+
+  Object.keys(real).forEach(methodName => {
+    proxy[methodName] = function() {
+      return (
+        real
+          // eslint-disable-next-line prefer-rest-params, no-unexpected-multiline
+          [methodName](...arguments)
+          .set('host', vhost)
+      );
+    };
+  });
+
+  return proxy;
+}
+
+// example of setting virtual host in supertest, used if
+// needing to support multiple domains
+// export const sitename = createVhostTester(app, 'example-host.com');
+
+export default createVhostTester(app, 'example-host.com');
