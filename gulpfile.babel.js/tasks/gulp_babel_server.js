5d084dd531238af1d780553822571055f2a708f0
michaelbenin <michaellouisbenin@gmail.com>
2017-07-14 16:28:06 +0000

    [BUGFIX] - Issue #46 - server side babel resiliency

--- gulpfile.babel.js/tasks/gulp_babel_server.js
+++ gulpfile.babel.js/tasks/gulp_babel_server.js
@@ -1,27 +1,34 @@
 import gulp from 'gulp';
+import { log } from 'gulp-util';
 import changed from 'gulp-changed';
 import babel from 'gulp-babel';
 import config from './../configs/config';
 
 gulp.task('babel-server', function babelServer() {
+  const babelStream = babel({
+    sourceMaps: 'inline',
+    presets: [
+      'react',
+      [
+        'env',
+        {
+          targets: {
+            node: 'current'
+          }
+        }
+      ]
+    ]
+  });
+
+  babelStream.on('error', function handleError(err) {
+    log(`Error transpiling babel in gulp_babel_server task.`);
+    log(err);
+    babelStream.end();
+  });
+
   return gulp
     .src(config.server.src)
     .pipe(changed(config.dest))
-    .pipe(
-      babel({
-        sourceMaps: 'inline',
-        presets: [
-          'react',
-          [
-            'env',
-            {
-              targets: {
-                node: 'current'
-              }
-            }
-          ]
-        ]
-      })
-    )
+    .pipe(babelStream)
     .pipe(gulp.dest(config.dest));
 });
