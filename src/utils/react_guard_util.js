24d606a4f9f6838e0f1ab90e608d7bb96b8c31cd
michaelbenin <michaellouisbenin@gmail.com>
2017-07-29 17:16:52 +0000

    [BUGFIX] - logging in react-guard-util

--- src/utils/react_guard_util.js
+++ src/utils/react_guard_util.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import reactGuard from 'react-guard';
+import get from 'lodash/get';
+
 import log from '../services/logger_service';
 import Error from '../views/containers/pages/error_page/error_page';
 
@@ -7,13 +9,8 @@ export default function(env) {
   // Catch and process component render exceptions.
   reactGuard(React, function handleError(err, componentInfo) {
     // Print stacktrace to the console
-    log.fatal(
-      `Error rendering: ${err.message}, in ${componentInfo.displayName}`
-    );
-    log.info(componentInfo.state, 'Component State');
-    log.info(componentInfo.props, 'Component Props');
-    log.fatal(err.stack);
-
+    log.fatal(`Error rendering: ${get(err, 'message')}`);
+    log.fatal(get(err, 'stack'));
     // Replace failed component with "Failed to render".
     // Use `return null` to render nothing.
     return <Error env={env} componentInfo={componentInfo} err={err} />;
